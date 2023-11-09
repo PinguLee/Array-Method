7714e958c6987fada713b63324c7e763b70d7c2e
Ho Nyeong <dev.honing@gmail.com>
2023-11-10 00:12:48 +0900

    Feat: 입력창에 포커스 기능 추가

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const form = document.getElementById('qna');
   const questionDiv = document.getElementById('question');
   const resultDiv = document.getElementById('result');
+  const answerInput = document.getElementById('answer');
   let questionIndex = null;
 
   const getRandomQuestion = () => {
@@ -10,6 +11,7 @@ document.addEventListener('DOMContentLoaded', () => {
       .then((data) => {
         questionDiv.textContent = data.question;
         questionIndex = data.index;
+        answerInput.focus(); // 답 입력 창에 자동으로 포커스를 맞춤
       })
       .catch((error) => {
         console.error('Error fetching random question:', error);
@@ -18,35 +20,35 @@ document.addEventListener('DOMContentLoaded', () => {
 
   getRandomQuestion();
 
-  form.addEventListener('submit', (event) => {
+  form.addEventListener('submit', async (event) => {
     event.preventDefault();
-    
-    const answer = document.getElementById('answer').value;
-    const answerInput = document.getElementById('answer');
 
-    fetch('/checkAnswer', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ index: questionIndex, answer }),
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        if (data.result) {
-          alert('정답');
-          answerInput.value = ''; // 입력된 답안 초기화
-          resultDiv.innerHTML = ''; // 결과 문자열 초기화
-          getRandomQuestion();
-        } else {
-          // If the answer is incorrect, display the exampleCode
-          resultDiv.innerHTML = data.exampleCode.replace(/\n/g, '<br>');
-          alert('오답');
-          answerInput.value = ''; // 입력된 답안 초기화
-        }
-      })
-      .catch((error) => {
-        console.error('Error checking answer:', error);
+    const answer = answerInput.value;
+
+    try {
+      const response = await fetch('/checkAnswer', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ index: questionIndex, answer }),
       });
+
+      const data = await response.json();
+
+      if (data.result) {
+        alert('정답');
+        answerInput.value = '';
+        resultDiv.innerHTML = '';
+        getRandomQuestion();
+      } else {
+        resultDiv.innerHTML = data.exampleCode.replace(/\n/g, '<br>');
+        alert('오답');
+        answerInput.value = '';
+        answerInput.focus(); // 오답일 경우 다시 답 입력 창에 포커스를 맞춤
+      }
+    } catch (error) {
+      console.error('Error checking answer:', error);
+    }
   });
 });
